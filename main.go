c55ceaf25e95175230bf65e5f25151b305e6acf5
seryegas <sergeu.shakirov.flat@gmail.com>
2025-02-03 09:56:30 +0300

    fix

--- main.go
+++ main.go
@@ -1,29 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"math"
-	"net/http"
-	"strconv"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Привет интернет!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
-
-	http.HandleFunc("/echo", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, request.URL.Query().Get("message"))
-	})
-
-	http.HandleFunc("/calculate-area", func(writer http.ResponseWriter, request *http.Request) {
-		radius, err := strconv.ParseFloat(request.URL.Query().Get("radius"), 64)
-		if err != nil {
-			panic(err)
-		}
-		area := math.Pi * math.Pow(radius, 2.0)
-		fmt.Fprint(writer, fmt.Sprintf("Площадь окружности с радиусом %s равна %f", request.URL.Query().Get("radius"), area))
-	})
-
-	http.ListenAndServe(":80", nil)
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
